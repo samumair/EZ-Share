7521d6b39534f192d9236dfb0f8f4bdfe4954621
samumair <77830446+samumair@users.noreply.github.com>
2022-04-21 16:17:59 +0200

    Add files via upload

---
+++ EZ-Share.ps1
@@ -0,0 +1,19 @@
+﻿#variables for share, will be modified later by GUI
+$folderpath="C:\Users\Samuel\Desktop" #must be a fully qualified path
+$user=('Samuel')
+
+$run=$True
+
+if ($run -eq $True)
+{
+    Write-Output "create share"
+    New-SmbShare -Name EZ-Share -Path $folderpath -Temporary -EncryptData $True -ReadAccess $user
+}
+
+if ($run -notlike $True)
+{
+    Write-Output "delete share"
+    Remove-SmbShare -Name EZ-Share -Force
+}
+
+Get-SmbShare 
