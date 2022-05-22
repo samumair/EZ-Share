93cf20f7a92971397e9796b590a5b08cd97ba398
samumair <77830446+samumair@users.noreply.github.com>
2022-05-22 11:13:56 +0200

    Add files via upload

--- EZ-Share.ps1
+++ EZ-Share.ps1
@@ -6,9 +6,10 @@
 [System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
 
 if(!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
- Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
+ Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList "-WindowStyle Hidden -File `"$($MyInvocation.MyCommand.Path)`"  `"$($MyInvocation.MyCommand.UnboundArguments)`""
  Exit
 }
+
 
 #-------------------------------------------------------------------------------------------
 #Window
