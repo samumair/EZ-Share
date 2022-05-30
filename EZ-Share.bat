5b7e7743a7594f2217708a4424c17d747216582b
GgSr1 <98089921+GgSr1@users.noreply.github.com>
2022-05-30 19:34:26 +0200

    Update EZ-Share.bat

--- EZ-Share.bat
+++ EZ-Share.bat
@@ -1,8 +1,10 @@
 @echo off
-echo.>> EZ-Share.ps1
-FINDSTR /R /I /V "^$ Footer" EZ-Share.ps1 >> EZ-Share-temp.ps1
+echo. >> EZ-Share.ps1
+echo #END >> EZ-Share.ps1
+findstr /v "#END" EZ-Share.ps1 > EZ-Share-temp.ps1
 del EZ-Share.ps1
 copy EZ-Share-temp.ps1 EZ-Share.ps1
 del EZ-Share-temp.ps1
 PowerShell -ExecutionPolicy Bypass ./EZ-Share.ps1
+
 
