c46670a054d2c4a9b2955c546134e14cfa9c66d2
georg schneider <geschneider@tsn.at>
2022-05-30 15:05:42 +0200

    Update EZ-Share.bat

--- EZ-Share.bat
+++ EZ-Share.bat
@@ -1,5 +1,8 @@
-copy EZ-Share.ps1 EZ-Share-neu.ps1
+@echo off
+echo.>> EZ-Share.ps1
+FINDSTR /R /I /V "^$ Footer" EZ-Share.ps1 >> EZ-Share-temp.ps1
 del EZ-Share.ps1
-copy EZ-Share-neu.ps1 EZ-Share.ps1
-del EZ-Share-neu.ps1
+copy EZ-Share-temp.ps1 EZ-Share.ps1
+del EZ-Share-temp.ps1
 PowerShell -ExecutionPolicy Bypass ./EZ-Share.ps1
+
